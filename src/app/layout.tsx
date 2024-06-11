2a7107035f18f86015da88d59e362871a3156061
Sainath Guraka <sainath.guraka@3insys.com>
2024-06-11 22:10:39 +0530

    added inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({ children }: IRootLayout) {
 
   return (
     <html lang='en'>
+      <head>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <div className='main-page-wrapper'>
           <DefaulHeader />
